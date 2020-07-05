287fde5e52c1920fcd2b4fc6d13b438694cde5ac
ks2019875067 <48702265+ks2019875067@users.noreply.github.com>
2020-07-06 02:00:25 +0900

    Update input-rand-2value.sql

--- input-rand-2value.sql
+++ input-rand-2value.sql
@@ -1,9 +1,9 @@
 INSERT INTO randX
     WITH RECURSIVE
       fa( id, x, y) AS (
-      VALUES(1 , abs(random())%61+40, abs(random())%61+40) 
+      VALUES(1 , abs(random())%99+1, abs(random())%50+20)
       UNION ALL 
-      SELECT id+1 , abs(random())%61+40 , abs(random())%61+40 
+      SELECT id+1 , abs(random())%99+1 , abs(random())%50+20 
       FROM fa
       WHERE id<=1000
 	)
